1e8350e235558ba67d19b198ffd85e90b78b12c8
Kenta Kodama <kentakodama@gmail.com>
2018-04-04 17:51:25 -0700

    deck swiper installed

--- src/components/deck/gallery.js
+++ src/components/deck/gallery.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import * as firebase from 'firebase';
+import Swiper from 'react-native-deck-swiper';
 
 class Gallery extends React.Component {
 
-  render() {
-    return (
+  render () {
+    return(
+
       <View style={styles.container}>
-        <Text style={{marginTop: '20%', fontSize: 40 }}>This is the swiping page</Text>
+          <Swiper
+              cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+              renderCard={(card) => {
+                  return (
+                      <View style={styles.card}>
+                          <Text style={styles.text}>{card}</Text>
+                      </View>
+                  )
+              }}
+              onSwiped={(cardIndex) => {console.log(cardIndex)}}
+              onSwipedAll={() => {console.log('onSwipedAll')}}
+              cardIndex={0}
+              backgroundColor={'#4FD0E9'}
+              stackSize= {3}>
+              <Button
+                  onPress={() => {console.log('oulala')}}
+                  title="Press me">
+                  You can press me
+              </Button>
+          </Swiper>
       </View>
-    );
+    )
   }
 }
 
@@ -20,6 +41,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
 });
 
 export default Gallery
